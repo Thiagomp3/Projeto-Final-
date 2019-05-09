069f07cd2fe199faed0f9906edf11704ec628b65
Lucca Nazari <lucca.n17@hotmail.com>
2019-05-09 16:59:24 -0300

    mapa

--- Peter Kong.py
+++ Peter Kong.py
@@ -5,5 +5,33 @@ Created on Thu May  9 16:08:22 2019
 @author: lucca
 """
 
-print("comecou essa porra")
-print("oi")
+mapa= [[0,1,2],
+       [0]*10,
+       [0]*10,
+       [0]*10,
+       [0]*10,]
+
+linha=0
+for e in mapa:
+    for i in mapa[linha]:
+        if i == 0:
+            print("espaco vazio feature")
+        elif i == 1:
+            print("chao")
+        elif i == 2:
+            print("escada")
+    linha+=1
+    
+
+        
+    
+
+
+       
+       
+       
+    
+
+        
+        
+        
